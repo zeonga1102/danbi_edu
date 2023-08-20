83fcbdb10a856031ffba4a172aa3692ee6326074
zeonga1102 <dlwjddk4587@naver.com>
2023-08-20 13:33:10 +0900

    FIX: static 경로 지정

--- danbi_edu/urls.py
+++ danbi_edu/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -7,4 +9,6 @@ urlpatterns = [
 
     path("", include("user.urls")),
     path("task/", include("task.urls")),
+
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
